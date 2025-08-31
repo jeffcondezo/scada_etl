54eb651c0cb490091808e461fb7d3b30bfa9b8e5
jeffcondezo <jeff_carlos_3040@hotmail.com>
2025-08-31 02:02:32 -0500

    Ethical 6

--- static/js/sensores_list.js
+++ static/js/sensores_list.js
@@ -3,7 +3,7 @@
 $(document).ready(function() {
             $('#tabla-sensores').DataTable({
                 language: {
-                    url: '//cdn.datatables.net/plug-ins/1.13.7/i18n/es-ES.json'
+                    url: '/static/js/es-ES.json'
                 }
             });
         });
